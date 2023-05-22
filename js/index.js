c8ee07ad631e1f7b844fa665caf325a8ac9b373d
AnarXan <111398750+AnarXan@users.noreply.github.com>
2023-05-22 22:20:32 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,228 @@
+//false - 0, NaN, undefined, null
+
+//true - 1, string, float, double, char, integer(number)
+
+
+//binary: ++ -- += -= x= /= %=
+
+//unar: + - x / %
+
+
+//mentiqi: <, >, <=, >=, ==, ===, !=(ferqlidirmi),
+//  || , &&, !  
+
+// console.log(0.1*0.1);
+
+// let x;
+
+// const result = x === undefined;
+
+// console.log(result);
+
+// let a = 1;
+// a*=10
+//  a = 1 * 10
+// console.log(a);
+
+// let a = 1;
+// a--;
+// a = 1-1
+// console.log(a);
+
+// console.log(2.2 + 0.2)
+
+// let log = console.log;
+
+// log(!!true) // true
+// log(!!"")  // false  ""  isaresi 0-dir (cavab false)
+// log(!!1)  // true
+// log(!!-1) // true
+// log(!!NaN)  // false
+// log(!!undefined)  // false
+// log(!!"false")  // true
+// log(!!null)  // false
+// log(+"1")  // 1
+// log(+true) // 1
+// log(+false) // 0
+// log(+"") // 0
+// log(+"hello") // NaN
+// log(+1) // 1
+
+// a = true
+// console.log(typeof a)
+
+
+// let var1;
+// console.log(var1);
+// console.log(typeof var1);
+
+// undefined tipi undefinedir
+
+// let var2 = null;
+// console.log(var2);
+// console.log(typeof var2);
+
+//  null tipi object-dir
+
+// a=5
+
+// document.write(a)
+
+// function info(){
+//     console.log(3);
+//     document.write("hello")
+// }
+
+// info();
+
+// function test(){
+//     return true
+// }
+
+// console.log(test());
+
+// function test(a){
+// console.log(a);
+// }
+
+// test(10);
+
+// function toplama(a,b){
+//     console.log(a+b);
+// }
+
+// toplama(2,3)
+
+
+// var a=2
+// var b=3
+
+// function cixma(){
+//     console.log(b+a);
+// }
+
+// cixma();
+
+// var a=parseInt(prompt("a reqemini daxil edin")) 
+// var b=parseInt(prompt("b reqemini daxil edin")) 
+
+// var a=parseFloat(prompt("a reqemini daxil edin")) 
+// var b=parseFloat(prompt("b reqemini daxil edin")) 
+
+// function riyazi(){
+//     console.log(a+b);
+// }
+
+// riyazi();
+
+
+// var a = parseInt(prompt("terefi daxil edin"))
+
+// function kvadrat(){
+//     console.log("sahe = "+ a*a);
+// }
+
+// kvadrat();
+
+
+// var a = parseInt(prompt("terefi daxil edin"))
+
+// function perimetr(){
+//     console.log("perimetr = " + a*4);
+// }
+
+// perimetr()
+
+
+// var inc = parseFloat(prompt("inc-i daxil edin"))
+
+// function santimetr(){
+//     console.log("sm = " + inc*0.56);
+// }
+// santimetr()
+
+
+// var pi = 3.14
+// var r = parseInt(prompt("radiusu daxil edin:"))
+
+// function daire(){
+//     console.log("sahe = " + 2*pi*r);
+// }
+
+// daire()
+
+
+// kvadratin dioqanali ist terefinden verilir. kvadratn sahesini tapan algoritm  s=0.5*d*d
+// duzbucaqli a ve b terefi ist terefinden verilir. duzbucaqlinin sahesini ve perimetrni tapan algoritm sahe a*b perimetr 2(a+b)
+// ucbucaq a terfi ve hundurluyu user terefinden verilir. ucbucagin sahesini tapan algoritm s=0.5a*h
+// dairenin radiusu user terefinden verilir. dairenin perimetri p=pi*r*r
+// konusun radiusu ve hundurluyu user terefinden verilir. hecimi tapan algoritm V=0.3*pi*r*r*h
+// faranheyt santiqrat 1f = s?
+
+
+// // exc 6
+
+// var f = parseInt(prompt("Farenheyti daxil edin: "))
+
+// function derece(){
+//     console.log("santiqrat: " + 5/9 * (f-32));
+// }
+// derece()
+
+// // exc 5
+
+// var r = parseInt(prompt("radiusi daxil edin:"))
+// var h = parseInt(prompt("hundurluyu daxil edin:"))
+// var pi = 3.14
+
+// function konus(){
+//     console.log("konusun hecmi: " + 0.3*pi*r*r*h);
+// }
+// konus()
+
+
+ 
+// // exc 4
+
+// var r = parseInt(prompt("radiusu daxil edin:"))
+// var pi = 3.14
+
+// function daire(){
+//     console.log("dairenin perimetri: " + pi*r*r );
+// }
+// daire()
+
+
+
+
+// exc 3
+
+// var a = parseInt(prompt("a terefi: "))
+// var hundurluk = parseInt(prompt("hundurluyu daxil edin:"))
+
+// function sahe(){
+//     console.log("sahesi: " + 0.5*a*hundurluk);
+// }
+// sahe()
+
+// exc 2
+
+// var a = parseInt(prompt("1ci terfi daxil edin: "))
+// var b = parseInt(prompt("2ci terfi daxil edin: "))
+
+// function saheVePerimetr(){
+//     console.log("sahesi: " + a*b);
+//     console.log("perimetr: " + 2*(a+b));
+// }
+// saheVePerimetr()
+
+// exc1
+
+// var d = parseInt(prompt("dioqanali daxil edin: "))
+// function sahe(){
+//     console.log("sahe = " + 0.5*d*d);
+// }
+// sahe()
+
+
+
